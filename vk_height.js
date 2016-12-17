97583604580e91f91fd4427c4db3b7c06e4a0254
Tem-Shop <mks_n@mail.ru>
2016-12-18 00:08:29 +0300

    Signed-off-by: Tem-Shop <mks_n@mail.ru>

--- vk_height.js
+++ vk_height.js
@@ -10,9 +10,4 @@ VK.callMethod('resizeWindow', 807, $('body').height() + 80);
 VK.callMethod('scrollWindow', 0);
 }
 setInterval('init_vk()', 200);*/
-VK.init(
-  function(){
-	  console.log("ASDASDASD");
-	  VK.callMethod("scrollWindow", 200, 500);
-	  VK.callMethod("resizeWindow", 500, 500);
-});
+
