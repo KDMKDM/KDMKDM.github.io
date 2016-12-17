049644f944e63926178ad438f7a1e4be82abb203
Tem-Shop <mks_n@mail.ru>
2016-12-17 21:29:23 +0300

    Signed-off-by: Tem-Shop <mks_n@mail.ru>

--- vk_height.js
+++ vk_height.js
@@ -1,6 +1,17 @@
-VK.init(
+vk {
+overflow-y: scroll;
+height: 800px;
+}
+
+VK.init(function() {});
+function init_vk() {
+VK.callMethod('resizeWindow', 807, $('body').height() + 80);
+VK.callMethod('scrollWindow', 0);
+}
+setInterval('init_vk()', 200);
+/*VK.init(
   function(){
 	  VK.callMethod("scrollWindow", 200, 500);
 	  VK.callMethod("resizeWindow", 500, 500);
 });
-    
+   */ 
